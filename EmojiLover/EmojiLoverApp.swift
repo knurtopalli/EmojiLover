55ac07294b9f077199d82d419b62076edf325d72
nur.com <nurtopalli@gmail.com>
2023-06-21 16:30:17 +0200

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Nur Topalli on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
